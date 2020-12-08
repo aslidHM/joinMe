6ca097cb7702b929c4689d80d6d83f3adf94b2e9
HM\ASLID <asa.lindkvist@hm.com>
2020-12-08 15:43:48 +0100

    Initializing, create support classes

--- src/main/java/com/example/joinMe/joinMeController.java
+++ src/main/java/com/example/joinMe/joinMeController.java
@@ -1,5 +1,24 @@
 package com.example.joinMe;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class joinMeController {
-    //test
+
+
+
+    @GetMapping("/activities/{memberID}")
+    public String activities (Model model) {
+
+        List<Activity> member = new ArrayList<>();
+        // todo get all customersmodel.addAttribute("customers", customers);
+
+        return "customers";
+    }
 }
