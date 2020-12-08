c0177b451fa9da16225a77dab885deddd719ea17
Yusri <yusri.shakir@gmail.com>
2020-12-08 13:45:31 +0100

    New project, joinme

---
+++ src/main/java/com/example/joinMe/joinMeController.java
@@ -0,0 +1,5 @@
+package com.example.joinMe;
+
+public class joinMeController {
+    //test
+}
---
+++ src/test/java/com/example/joinMe/JoinMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joinMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
